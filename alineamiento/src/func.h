ceb5d731345e59dcb57689063472a32559e74870
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-03-21 13:36:51 -0500

    print char ***

--- alineamiento/src/func.h
+++ alineamiento/src/func.h
@@ -11,4 +11,19 @@ void print_matrix(T **matrix, unsigned _n, unsigned _m){
 	}
 }
 
+template<class T>
+void print_matrix(T ***matrix, unsigned _n, unsigned _m, unsigned _l){
+	for(unsigned i=0; i<_n; i++){		
+		for(unsigned j=0; j<_m; j++){
+			std::cout << "[";
+			for(unsigned k=0; k<_l; k++){
+				std::cout << matrix[i][j][k];
+				if(k!=_l-1)	std::cout << " ";
+			}
+			std::cout << "] \t";
+		}
+		std::cout << "\n";
+	}
+}
+
 #endif
