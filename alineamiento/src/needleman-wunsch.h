4d98dc16b1463a3253ddcc119f98fb41021c39a8
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-03-21 13:37:45 -0500

    Create Back Matrix and Save this Matrix m_back

--- alineamiento/src/needleman-wunsch.h
+++ alineamiento/src/needleman-wunsch.h
@@ -7,7 +7,17 @@
 #include <algorithm>
 #include "func.h"
 
-extern unsigned TMAX;
+#define TMAX 1000
+#define PATH 6
+
+
+/*	
+	1   2
+	 \  |
+	  \ |
+	0---1
+*/
+
 
 class TNeedlemanWunsch{
 private:
@@ -15,15 +25,17 @@ private:
 	unsigned n, m;				// matrix
 	int match_score;			// match score
 	int mismatch_score;			// mismatch score
-	int gap_score;				// gap score
+	int gap_score;				// gap score	
 public:	
 	int **m_dp;					// matrix
+	char ***m_back;				// matrix to do the traceback
 
 	TNeedlemanWunsch(std::string, std::string, int, int, int);
 	~TNeedlemanWunsch();
 
 	int FMakeMatrix();
-	void FPrintMatrix();
+	void FPrintWeightMatrix();
+	void FPrintBackMatrix();
 	std::pair<std::string, std::string> FGlobalOptimum();
 	int FSimilarity(char, char);
 };
@@ -42,35 +54,63 @@ TNeedlemanWunsch::TNeedlemanWunsch(std::string _dnaa, std::string _dnab,
 	this->gap_score      = _gap;
 
 	if((this->n <  TMAX) and (this->m < TMAX)){
-		this->m_dp = new int*[this->n];
+		this->m_dp   = new int*[this->n];
+		this->m_back = new char**[this->n];
 		for(unsigned i=0; i<this->n; i++){
 			this->m_dp[i] = new int[this->m];
+			this->m_back[i] = new char*[this->m];
+			for(unsigned j=0; j<this->m; j++){
+				this->m_back[i][j] = new char[PATH];
+				for(unsigned k=0; k<PATH; k++){
+					this->m_back[i][j][k] = '0';
+				}					
+			}
 		}
-	}	
+	}
 }
 
 int TNeedlemanWunsch::FSimilarity(char _a, char _b){
 	return (_a == _b)? match_score: -mismatch_score;
 }
 
-void TNeedlemanWunsch::FPrintMatrix(){
+void TNeedlemanWunsch::FPrintWeightMatrix(){
 	print_matrix(m_dp, n, m);
+}
+
+void TNeedlemanWunsch::FPrintBackMatrix(){
+	print_matrix(m_back, n, m, PATH/2);
 }
 
 int TNeedlemanWunsch::FMakeMatrix(){
 	// Init Matrix
-	for(unsigned i=0; i<n; i++)	this->m_dp[i][0] = -i*this->gap_score;
-	for(unsigned j=0; j<m; j++)	this->m_dp[0][j] = -j*this->gap_score;
+	this->m_dp[0][0] = 0;
+	for(unsigned i=1; i<n; i++){
+		this->m_dp[i][0] = -i*this->gap_score;
+		this->m_back[i][0][0] = '1';
+	}
+	for(unsigned j=1; j<m; j++){
+		this->m_dp[0][j] = -j*this->gap_score;
+		this->m_back[0][j][2] = '1';
+	}
 
 	int tmp;
-	int mtmp;
+	int p,q,r,mx;
 
+	// Array [0,0,0,0,0,0]
 	for(unsigned i=1; i<n; i++){
 		for(unsigned j=1; j<m; j++){
 			tmp = FSimilarity(m_dna[0][i-1], m_dna[1][j-1]);
 
-			mtmp = std::max(m_dp[i-1][j]-gap_score, m_dp[i][j-1]-gap_score);
-			this->m_dp[i][j] = std::max(m_dp[i-1][j-1]+tmp, mtmp);
+			p = m_dp[i][j-1] - gap_score;		// horizontal 0
+			q = m_dp[i-1][j-1] + tmp;			// diagonal   1
+			r = m_dp[i-1][j] - gap_score;		// vertical	  2
+			mx = std::max(p,std::max(q,r));
+
+			if(p == mx)	this->m_back[i][j][0] = '1';
+			if(q == mx)	this->m_back[i][j][1] = '1';
+			if(r == mx)	this->m_back[i][j][2] = '1';
+			
+			this->m_dp[i][j] = mx;
 		}
 	}
 
@@ -78,12 +118,10 @@ int TNeedlemanWunsch::FMakeMatrix(){
 }
 
 std::pair<std::string, std::string> TNeedlemanWunsch::FGlobalOptimum(){
-	int i = n-1;
-	int j = m-1;
-
 	std::string align_a = "";
 	std::string align_b = "";
 
+	/*
 	while(i!=0 || j!=0){
 		if(i==0){
 			align_a = '-' + align_b;
@@ -120,7 +158,8 @@ std::pair<std::string, std::string> TNeedlemanWunsch::FGlobalOptimum(){
 				j--;
 			}
 		}
-	}	
+	}
+	*/
 
 	return std::make_pair(align_a, align_b);
 }
