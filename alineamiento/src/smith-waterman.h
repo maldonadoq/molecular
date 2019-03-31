b96a55856425fc0984a300f9e9737b8457ad24a7
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-03-30 20:28:35 -0500

    smith-waterman - all paths and penalty

--- alineamiento/src/smith-waterman.h
+++ alineamiento/src/smith-waterman.h
@@ -1,143 +1,178 @@
 #ifndef _SMITH_WATERMAN_H_
 #define _SMITH_WATERMAN_H_
 
-extern unsigned TMAX;
-
+#include <string.h>
+#include <iostream>
 #include <utility>
-#include "func.h"
+#include <vector>
+#include <queue>
+#include "alignment.h"
 
-class TSmithWaterman{
-private:
-	std::string m_dna[2];		// dna strings
-	unsigned n, m;				// matrix (row-col)
-	int match_score;			// match score
-	int mismatch_score;			// mismatch score
-	int gap_score;				// gap score
+#define TMAX 1000
+#define PATH 3
 
+class TSmithWaterman: public TAlignment{
 public:
-	int **m_dp;					// matrix
-	TSmithWaterman(std::string, std::string, int, int, int);
-	~TSmithWaterman();
+	int m_score;
+	TSmithWaterman(std::string _dnaa, std::string _dnab,int _match, int _mismatch, int _gap):
+		TAlignment(_dnaa,_dnab,_match,_mismatch,_gap){	};
+	~TSmithWaterman(){	};
 
-	void FMakeMatrix();
-	void FPrintMatrix();
-	int FSimilarity(char, char);
-	std::pair<int, int> FFindMax();
-	std::pair<std::string, std::string> FLocalOptimum();
+	int FMakeMatrix();
+	std::vector<std::pair<unsigned, unsigned> > FFindNMax(unsigned);
+	std::vector<std::pair<std::string,std::string> > FLocalOptimum(unsigned);
+	std::vector<std::vector<std::pair<int, int> > > FBFS(std::pair<unsigned, unsigned>, int);
 };
 
-TSmithWaterman::TSmithWaterman(std::string _dnaa, std::string _dnab,
-	int _match, int _mismatch, int _gap){
-	this->m_dna[0] = _dnaa;
-	this->m_dna[1] = _dnab;
+std::vector<std::pair<unsigned, unsigned> > TSmithWaterman::FFindNMax(unsigned _n){
+	std::vector<std::pair<unsigned, unsigned> > maxs;
 
-	this->n = this->m_dna[0].size()+1;
-	this->m = this->m_dna[1].size()+1;
-
-	this->match_score    = _match;
-	this->mismatch_score = _mismatch;
-	this->gap_score 	 = _gap;
-
-	if((this->n <  TMAX) and (this->m < TMAX)){
-		this->m_dp = new int*[this->n];
-		for(unsigned i=0; i<this->n; i++){
-			this->m_dp[i] = new int[this->m];
-		}
-	}
-}
-
-int TSmithWaterman::FSimilarity(char _a, char _b){
-	return (_a == _b)? match_score: -mismatch_score;
-	// return (_a == _b)? match_score: -gap_score;
-}
-
-void TSmithWaterman::FPrintMatrix(){
-	print_matrix(m_dp, n, m);
-}
-
-std::pair<int, int> TSmithWaterman::FFindMax(){
-	int tmax = 0;
-	int ii, jj;
-	for(unsigned i=1; i<n-1; i++){
-		for(unsigned j=1; j<m-1; j++){
-			if(m_dp[i][j] > tmax){
-				tmax = m_dp[i][j];
-				ii = i;
-				jj = j;
+	if(m_score > 0){
+		for(unsigned i=1; i<n; i++){
+			for(unsigned j=1; j<m; j++){
+				if(m_dp[i][j] == m_score){
+					maxs.push_back(std::make_pair(i,j));
+					if(maxs.size() > _n){
+						i = n;	j = m;
+					}
+				}
 			}
 		}
 	}
 
-	return std::make_pair(ii,jj);
+	return maxs;
 }
 
-void TSmithWaterman::FMakeMatrix(){
-	for(unsigned i=0; i<n; i++)	this->m_dp[i][0] = 0;
-	for(unsigned j=0; j<m; j++)	this->m_dp[0][j] = 0;
+int TSmithWaterman::FMakeMatrix(){
+	// Init Matrix
+	this->m_dp[0][0] = 0;
+	for(unsigned i=1; i<n; i++)	this->m_dp[i][0] = 0;
+	for(unsigned j=1; j<m; j++)	this->m_dp[0][j] = 0;
 
-	int tmp, tmp1, tmp2;
+	int tmp;
+	int p,q,r,mx;
+
+	this->m_score = 0;
+
+	// Array [0,0,0]
 	for(unsigned i=1; i<n; i++){
 		for(unsigned j=1; j<m; j++){
-			tmp  = FSimilarity(m_dna[0][i-1], m_dna[1][j-1]);
-			tmp1 = std::max(m_dp[i-1][j]-gap_score, m_dp[i][j-1]-gap_score);
-			tmp2 = std::max(m_dp[i-1][j-1] + tmp, tmp1);
-			this->m_dp[i][j] = std::max(tmp2, 0);
+			tmp = FSimilarity(m_dna[0][i-1], m_dna[1][j-1]);
+
+			p = m_dp[i][j-1] - gap_score;		// horizontal 0
+			q = m_dp[i-1][j-1] + tmp;			// diagonal   1
+			r = m_dp[i-1][j] - gap_score;		// vertical	  2
+			mx = std::max(p,std::max(q,std::max(r,0)));
+
+			if(mx>0){
+				if(p == mx)	this->m_back[i][j][0] = '1';
+				if(q == mx)	this->m_back[i][j][1] = '1';
+				if(r == mx)	this->m_back[i][j][2] = '1';
+			}
+			this->m_dp[i][j] = mx;
+			if(mx > m_score)
+				this->m_score = mx;
 		}
 	}
+
+	return m_score;
 }
 
-std::pair<std::string, std::string> TSmithWaterman::FLocalOptimum(){
-	std::pair<int, int> idx = FFindMax();
+std::vector<std::vector<std::pair<int, int> > > TSmithWaterman::FBFS(std::pair<unsigned, unsigned> _src, int _trg){
 
-	int i = idx.first;
-	int j = idx.second;
+	std::vector<std::vector<std::pair<int, int> > > mpaths;
+	std::queue<std::vector<std::pair<int, int> > >	mqueue;
+	std::vector<std::pair<int, int> > path;
+	std::vector<std::pair<int, int> > tpath;
 
-	std::string align_a = "";
-	std::string align_b = "";
+	path.push_back(_src);
+	mqueue.push(path);
 
-	while((i!=0 || j!=0) and m_dp[i][j] != 0){
-		if(i==0){
-			align_a = '-' + align_b;
-			align_b = m_dna[1][j-1] + align_b;
+	std::pair<int, int> last;
+	std::pair<int, int> tmp;
 
-			j--;
-		}
-		else if(j==0){
-			align_a = m_dna[0][i-1] + align_a; 
-			align_b = '-' + align_b;
+ 	while(!mqueue.empty()){
+		path = mqueue.front();
+		mqueue.pop();
 
-			i--;
+		last = path[path.size()-1];
+		if(m_dp[last.first][last.second] == _trg){
+			mpaths.push_back(path);
 		}
 		else{
-			int s = (m_dna[0][i-1] == m_dna[1][j-1]) ? match_score: -mismatch_score;
+			for(unsigned i=0; i<PATH; i++){
+				if(m_back[last.first][last.second][i] == '1'){
+					switch(i){
+						case 0:
+							tmp = std::make_pair(last.first,   last.second-1);
+							break;
+						case 1:
+							tmp = std::make_pair(last.first-1, last.second-1);
+							break;
+						case 2:
+							tmp = std::make_pair(last.first-1, last.second);
+							break;
+					}
 
-			if(m_dp[i][j] == m_dp[i-1][j-1]+s){
-				align_a = m_dna[0][i-1] + align_a;
-				align_b = m_dna[1][j-1] + align_b;
-
-				i--;
-				j--;
-			}
-			else if(m_dp[i-1][j] > m_dp[i][j-1]){
-				align_a = m_dna[0][i-1] + align_a;
-				align_b = '-' + align_b;
-
-				i--;
-			}
-			else{
-				align_a = '-' + align_a;
-				align_b = m_dna[1][j-1] + align_b;
-
-				j--;
+					if(is_not_visited(path, tmp)){
+						tpath = path;
+						tpath.push_back(tmp);
+						mqueue.push(tpath);
+					}
+				}
 			}
 		}
-	}	
+	}
 
-	return std::make_pair(align_a, align_b);
+	return mpaths;
 }
 
-TSmithWaterman::~TSmithWaterman(){
-	delete []m_dp;
+std::vector<std::pair<std::string,std::string> > TSmithWaterman::FLocalOptimum(unsigned _n){
+	std::vector<std::pair<std::string, std::string> > alignments;
+	std::vector<std::vector<std::pair<int, int> > > paths;
+	std::vector<std::vector<std::pair<int, int> > > tpaths;
+
+	std::vector<std::pair<unsigned, unsigned> > maxs = FFindNMax(_n);
+	std::vector<std::pair<int, int> > tpath;
+
+	for(unsigned i=0; i<maxs.size(); i++){
+		tpaths = FBFS(maxs[i], 0);
+		for(unsigned j=0; j<tpaths.size(); j++){
+			paths.push_back(tpaths[j]);
+		}
+	}
+
+	std::pair<std::string, std::string> alignm;
+	std::pair<unsigned, unsigned> tmp;
+
+	unsigned ii;
+	unsigned jj;
+
+	for(unsigned i=0; i<paths.size(); i++){
+		alignm.first  = "";
+		alignm.second = "";
+
+		tmp = paths[i][paths[i].size()-1];
+		ii = tmp.first;
+		jj = tmp.second;
+		
+		for(int j=paths[i].size()-2; j>=0; j--){
+			tmp = paths[i][j];
+			if(tmp.first == ii) alignm.first += '-';
+			else alignm.first += m_dna[0][tmp.first-1];
+
+			if(tmp.second == jj) alignm.second += '-';
+			else alignm.second += m_dna[1][tmp.second-1];
+
+ 			ii = tmp.first;	
+ 			jj = tmp.second;
+		}
+		alignments.push_back(make_pair(alignm.first, alignm.second));		
+	}
+
+	paths.clear();
+
+	return alignments;
 }
 
 #endif
