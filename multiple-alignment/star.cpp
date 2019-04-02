4a0cc1268bb66b557fc789356f77a8c967684311
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-01 21:25:26 -0500

    Star Heuristic

--- multiple-alignment/star.cpp
+++ multiple-alignment/star.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char const *argv[]){
 
 	std::vector<std::string> dnas;
+	std::vector<std::string> alignment;
 
 	dnas.push_back("ATTGCCATT");
 	dnas.push_back("ATGGCCATT");
@@ -15,11 +16,24 @@ int main(int argc, char const *argv[]){
 	int match    = 1;
 	int mismatch = 1;
 	int gap      = 2;
+	int score;
 
 	TStar *s = new TStar(match, mismatch, gap);
-	// s->FSetDna(dnas);
+	s->FSetDna(dnas);
+	s->FScores();	
+	alignment = s->FGetMultipleAlignment();
+
+	/*alignment.push_back("MQPILLLV");
+	alignment.push_back("MLR-LL--");
+	alignment.push_back("MK-ILLL-");
+	alignment.push_back("MPPVLILV");*/
+
+	score = s->FSumPairs(alignment);
+
+	std::cout << "[score]: " << score << "\n";
+	print_vector_t(alignment);
+	// s->FPrintScores();
 
 	delete s;
-	
 	return 0;
 }
