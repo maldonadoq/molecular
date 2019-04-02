dff139a90f27422c5c43d1bb68b07e61c2658c6b
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-04-01 21:27:06 -0500

    Pair score-alignmentgit add utils/score.h

--- utils/score.h
+++ utils/score.h
@@ -12,10 +12,24 @@ public:
 		this->m_alignment = _alignment;
 	}
 
+	TScoreAlignment(){
+		this->m_score = 0;
+		this->m_alignment = std::make_pair("","");
+	}
+
 	friend std::ostream& operator<< (std::ostream & out, TScoreAlignment &c){
 		out << "score: " << c.m_score << "\nalign1: " << c.m_alignment.first << "\nalign2: " << c.m_alignment.second;
 		return out;
    	}
 };
+
+std::string str_complete(std::string _dna, char _c, unsigned _size){
+	std::string dna = _dna;
+	if(_dna.size()<_size){
+		for(unsigned i=_dna.size(); i<_size; i++)
+			dna += _c;
+	}
+	return dna;
+}
 
 #endif
