075cc2226811758e3feb89ee877711e39531c288
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2019-05-24 11:08:35 -0500

    cluster update
    agglomerative and dissociative

--- cluster/agglomerative.cpp
+++ cluster/agglomerative.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 #include <chrono>
 #include <stdio.h>
+#include "src/cmp.h"
 #include "src/utils.h"
 #include "src/agglomerative.h"
 
 using namespace std::chrono;
 
-template<class T>
-class TC{
-public:
-	inline T operator()(T x, T y){
-		// return std::min(x,y);
-		// return std::max(x,y);
-		return (x+y)/2;
-	}
-};
-
 typedef TAgglomerative<float, std::string, TC<float> > Cluster;
 typedef std::vector<float> dvect;
 
+// build: g++ agglomerative.cpp -o agglomerative.out
 int main(int argc, char const *argv[]){
-	std::vector<std::string > headers;
-	std::vector<dvect>        distances;
-
-	std::string filename = "../data/Diauxic.txt";
-	pre_processing_file(filename, distances, headers);
-
-	/*int nc = 7;
+	int nc = 7;
 	std::vector<std::string > headers   = {"A","B","C","D","E","F","G"};
 	std::vector<dvect>        distances = std::vector<dvect>(nc, dvect(nc));
+	std::vector<std::string > clusters;
 
 	distances[0] = {0.00};
 	distances[1] = {2.15, 0.00};
@@ -36,11 +23,9 @@ int main(int argc, char const *argv[]){
 	distances[3] = {1.07, 1.14, 0.43, 0.00};
 	distances[4] = {0.85, 1.38, 0.21, 0.29, 0.00};
 	distances[5] = {1.16, 1.01, 0.55, 0.22, 0.41, 0.00};
-	distances[6] = {1.56, 2.83, 1.86, 2.04, 2.02, 2.05, 0.00};*/
+	distances[6] = {1.56, 2.83, 1.86, 2.04, 2.02, 2.05, 0.00};
 
-	unsigned ncluster = 250;
-
-	// std::cout << "cluster's number: "; std::cin >> ncluster;
+	unsigned ncluster = 3;
 
 	Cluster *cl = new Cluster();
 
@@ -51,10 +36,13 @@ int main(int argc, char const *argv[]){
 	cl->Init(distances, headers);	
 
 	tinit = high_resolution_clock::now();
-		cl->Cluster(ncluster);
+		cl->Run(ncluster);
 	tend = high_resolution_clock::now();
 	time_span = duration_cast<duration<double>>(tend - tinit);
 	std::cout << "[cluster time] : " << time_span.count() << " s\n";	
+
+	clusters = cl->GetClusters();
+	print_vector(clusters);
 
 	delete cl;
 
--- cluster/dissociative.cpp
+++ cluster/dissociative.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <stdio.h>
+#include "src/utils.h"
 #include "src/dissociative.h"
 
 using namespace std::chrono;
@@ -8,11 +9,12 @@ using namespace std::chrono;
 typedef TDissociative<float, std::string> Cluster;
 typedef std::vector<float> dvect;
 
+// build: g++ dissociative.cpp -o dissociative.out
 int main(int argc, char const *argv[]){
-	
 	int nc = 7;
 	std::vector<std::string > headers   = {"A","B","C","D","E","F","G"};
 	std::vector<dvect>        distances = std::vector<dvect>(nc, dvect(nc));
+	std::vector<std::string > clusters;
 
 	distances[0] = {0.00};
 	distances[1] = {2.15, 0.00};
@@ -21,6 +23,9 @@ int main(int argc, char const *argv[]){
 	distances[4] = {0.85, 1.38, 0.21, 0.29, 0.00};
 	distances[5] = {1.16, 1.01, 0.55, 0.22, 0.41, 0.00};
 	distances[6] = {1.56, 2.83, 1.86, 2.04, 2.02, 2.05, 0.00};
+	unsigned ncluster = 3;
+
+	std::vector<std::string> cluster;
 
 	Cluster *cl = new Cluster();
 
@@ -29,12 +34,16 @@ int main(int argc, char const *argv[]){
 	duration<double> time_span;
 
 	cl->Init(distances, headers);
+	cl->SetType(0);
 
 	tinit = high_resolution_clock::now();
-		cl->Cluster();
+		cl->Run(ncluster);
 	tend = high_resolution_clock::now();
 	time_span = duration_cast<duration<double>>(tend - tinit);
 	std::cout << "[cluster time] : " << time_span.count() << " s\n";
+
+	clusters = cl->GetClusters();
+	print_vector(clusters);
 	delete cl;
 	
 	return 0;
